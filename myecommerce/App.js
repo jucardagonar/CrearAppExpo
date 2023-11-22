966c2f4c87a77437c4febb6fa922540e9cb313ed
Juan Carlos González Armata <jucardagonar@gmail.com>
2023-11-22 18:37:19 -0300

    prueba

---
+++ myecommerce/App.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import MainScreen from "../src/components/screens/MainScreen";
+
+export default function App() {
+  return <MainScreen />;
+}
+
+const styles = StyleSheet.create({});
